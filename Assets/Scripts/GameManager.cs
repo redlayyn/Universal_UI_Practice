7c866a75a79a17cf4b1b9a3a506c26e9e04138e8
redlayyn <k.mazhlisov@mail.ru>
2026-06-09 16:50:55 +0300

    Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("HUD Ёлементы")]
+    [SerializeField] private Slider hpSlider;
+    [SerializeField] private TextMeshProUGUI coinsText;
+
+    private float currentHP = 100f;
+    private float maxHP = 100f;
+    private int currentCoins = 100;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    public void ChangeHP(float amount)
+    {
+        currentHP += amount;
+        currentHP = Mathf.Clamp(currentHP, 0f, maxHP);
+
+        hpSlider.DOValue(currentHP / maxHP, 0.3f);
+    }
+
+    public void AddCoins(int amount)
+    {
+        currentCoins += amount;
+        if (currentCoins < 0) currentCoins = 0;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        coinsText.text = currentCoins.ToString();
+    }
+}
